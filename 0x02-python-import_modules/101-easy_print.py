896a2189156ea446dd43f50814b54871220b9266
Bobybuu <c43057772@gmail.com>
2023-12-29 03:14:06 -0800

    101-easy_print.py

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
-__import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))
+from magic_calculation_102 import add, sub
 
+
+def magic_calculation(a, b):
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return (c)
+    else:
+        return sub(a, b)
